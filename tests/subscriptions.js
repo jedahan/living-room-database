c5f2129707d73bd81380c65108973208d52192c5
Jonathan Dahan <hi@jonathan.is>
2018-03-29 14:21:47 -0400

    add retraction test

--- tests/subscriptions.js
+++ tests/subscriptions.js
@@ -3,7 +3,7 @@ const RoomDB = require('../dist/roomdb.js')
 
 const gorogInitial = `gorog is a barbarian at 40, 50`
 
-test('assert adds to the log', async t => {
+test('assertions are listened to', async t => {
   const room = new RoomDB()
   const client = room.client()
 
@@ -18,8 +18,36 @@ test('assert adds to the log', async t => {
       resolve()
     })
   })
-  
+
   client.assert(gorogInitial)
+  await client.flushChanges()
+  await subscription
+})
+
+test('retractions are listened to', async t => {
+  const room = new RoomDB()
+  const client = room.client()
+
+  let emissions = 0
+
+  const subscription = new Promise((resolve, reject) => {
+    client.subscribe(['$name is a $class at $x, $y'], ({assertions, retractions, selections}) => {
+      emissions++
+      if (emissions === 2) {
+        const gorogFact = retractions[0]
+        t.deepEqual(assertions, [])
+        t.deepEqual(gorogFact.name.value, 'gorog')
+        t.deepEqual(gorogFact.class.value, 'barbarian')
+        t.deepEqual(gorogFact.x.value, 40)
+        t.deepEqual(gorogFact.y.value, 50)
+        resolve()
+      }
+    })
+  })
+
+  client.assert(gorogInitial)
+  await client.flushChanges()
+  client.retract(gorogInitial)
   await client.flushChanges()
   await subscription
 })
